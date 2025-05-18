9fea78777a40d97af5790bf4f4f7c8866327a08e
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-18 10:09:37 -0400

    Added binary operator support for Verctors on either side of the add, subtract, and multiplication symbols

--- vector.py
+++ vector.py
@@ -44,6 +44,8 @@ class Vector:
   
   def __add__(self, other):
     """
+    For when our Vector is on the right side of the add symbol.
+
     Args:
       other: To be added to this Vector. Has to be of type Vector, int, or float.
     
@@ -57,9 +59,28 @@ class Vector:
     else:
       raise TypeError(f"Type 'Vector' cannot be added to type '{type(other)}'.")
   
+  def __radd__(self, other):
+    """
+    For when our Vector is on the right side of the add symbol.
+
+    Args:
+      other: To be added to this Vector. Has to be of type Vector, int, or float.
+    
+    Returns:
+      A new Vector that results from adding other to this Vector.
+    """
+    if isinstance(other, Vector):
+      return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+    elif isinstance(other, (int, float)):
+      return Vector(self.x + other, self.y + other, self.z + other)
+    else:
+      raise TypeError(f"Type '{type(other)}' cannot be added to type 'Vector'.")
+  
 
   def __sub__(self, other):
     """
+    For when our Vector is on the left side of the add symbol.
+
     Args:
       other: To be subtracted from this Vector. Has to be of type Vector, int, or float.
     
@@ -73,6 +94,23 @@ class Vector:
     else:
       raise TypeError(f"Type '{type(other)}' cannot be subtracted from type 'Vector'.")
   
+  def __rsub__(self, other):
+    """
+    For when our Vector is on the right side of the add symbol.
+
+    Args:
+      other: To be subtracted from this Vector. Has to be of type Vector, int, or float.
+    
+    Returns:
+      A new Vector that results from subtractiong other from this Vector.
+    """
+    if isinstance(other, Vector):
+      return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+    elif isinstance(other, (int, float)):
+      return Vector(self.x - other, self.y - other, self.z - other)
+    else:
+      raise TypeError(f"Type 'Vector' cannot be subtracted from type '{type(other)}'.")
+  
 
   def invert(self):
     """
@@ -83,5 +121,48 @@ class Vector:
       A new Vector whose components are the negative versions of this Vector's components.
     """
     return Vector(-1*self.x, -1*self.y, -1*self.z)
+
+  def __mul__(self, other):
+    """
+    For when our Vector is on the left side of the multiplication symbol.
+
+    Args:
+      other: A scalar value, that will be multiplied to each component of this vector. Has to be of type int or float
+      
+
+    Returns:
+      A new vector, which is just (other * [the self vector])
+    """
+    if isinstance(other, (int, float)):
+      return Vector(self.x * other, self.y * other, self.z * other)
+    else:
+      raise TypeError(f"Type 'Vector' cannot be multiplied by non scalar type {type(other)}.")
+  
+  def __rmul__(self, other):
+    """
+
+    For when our Vector is on the right side of the multiplication symbol.
+
+    Args:
+      other: A scalar value, that will be multiplied to each component of this vector. Has to be of type int or float
+      
+
+    Returns:
+      A new vector, which is just (other * [the self vector])
+    """
+    if isinstance(other, (int, float)):
+      return Vector(self.x * other, self.y * other, self.z * other)
+    else:
+      raise TypeError(f"Type 'Vector' cannot be multiplied by non scalar type {type(other)}.")
+  
+  def euclideanDistance(self, other):
+    """
+    Args:
+      other: A Vector. We will take the Euclidean distance between the invoking Vector and the 'other' Vector.
+    
+    Returns:
+      A scalar value, most likely float, that represents the Euclidean distance between the invoking Vector and the 'other' Vector.
+    """
+    pass
   
 
