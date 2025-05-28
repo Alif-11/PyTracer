90b8502a6ed38bae9b6416f92af8be289b5c4735
Alif Abdullah <alifabdullah7040@gmail.com>
2025-05-28 10:34:33 -0400

    Created color, object, and ray files ot continue the raytracing algorithm development

---
+++ objects.py
@@ -0,0 +1,7 @@
+# This file contains all the objects that I want to use for raytracing
+
+class Sphere:
+  def __init__(self, center, radius, color):
+    self.center = center
+    self.radius = radius
+    self.color = color
---
+++ ray.py
@@ -0,0 +1,14 @@
+import math
+import numpy as np
+
+from vector import Vector
+from color import Color
+from objects import Sphere
+
+class Intersection:
+
+  @staticmethod
+  def nearest_intersection(list_of_intersections):
+    """
+    Returns the nearest intersection between a ray and an object."""
+    pass
